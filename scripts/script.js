4c1130f422c8db624ee84e979982fa1bc82a1bb9
SpunkyAardvark123 <ktsanter@gmail.com>
2017-04-17 15:49:04 -0400

    filepicker early draft

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,23 @@
 "use strict";
 
 $(document).ready(function() {
+  $("#files").on("change", function() {
+    handleFileSelect(this.files);
+  })
 });
+
+function handleFileSelect(files) {
+  console.log('handleFileSelect');
+  var s = "<div>";
+  console.log('file handler: files.length=' + files.length);
+  for (var i = 0, f; f = files[i]; i++) {
+    s += 'name=' + f.name + '<br/>';
+    s += ' type=' + (f.type || 'n/a') + '<br/>';
+    s += ' size=' + f.size + '<br/>';
+    s += ' date=' + (f.lastModifiedData ? f.lastModifiedDate.toLocaleDateString() : 'n/a') + '<br/>';
+    s += ' path=' + f.webkitRelativePath;
+                
+  }
+  s += "</div>";
+  $("#list").html(s);
+}
