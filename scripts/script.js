7f7a4866b8347f5192348cf7b64d8f7e1f5b2d7e
SpunkyAardvark123 <ktsanter@gmail.com>
2017-04-17 20:21:34 -0400

    form POST working

--- scripts/script.js
+++ scripts/script.js
@@ -4,10 +4,15 @@ $(document).ready(function() {
   $("#files").on("change", function() {
     handleFileSelect(this.files);
   })
+  
+  $("#btnUpload").click(function() {
+    handleUploadClick();
+  });
 });
 
 function handleFileSelect(files) {
   console.log('handleFileSelect');
+  /*
   var s = "<div>";
   console.log('file handler: files.length=' + files.length);
   for (var i = 0, f; f = files[i]; i++) {
@@ -20,4 +25,41 @@ function handleFileSelect(files) {
   }
   s += "</div>";
   $("#list").html(s);
+  upload(files[0]);
+  */
+}
+
+function upload(f) {
+ $.ajax({
+    url: f,
+    dataType: "text",
+    success: function(data) {
+      console.log('read data...');
+      upload2(data);
+    },
+    error: function() {
+      console.log('unable to load from comment data file "' + f.name + '"');
+    }
+  });
+}
+
+function upload2(data) {
+  $.ajax({
+    url: "/commentbuddy/user/data",
+    type: "POST",
+    contentType: text,
+    data: data,
+    cache:false,
+    success: function(data) {
+      console.log('upload succeeded');
+    },
+     error: function (jqXHR,  textStatus,  errorThrown ) {
+       console.log('failed to upload comment file');
+       console.log(textStatus + '\n' + errorThrown);
+     }
+  });
+}
+
+function handleUploadClick() {
+  
 }
