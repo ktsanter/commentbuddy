8a4e4ac7a67b89312e7e0e6bcdde058a4a488331
SpunkyAardvark123 <ktsanter@gmail.com>
2018-04-22 20:01:23 -0400

    reworking front end

--- scripts/parse.js
+++ scripts/parse.js
@@ -1,58 +1,105 @@
 "use strict";
-function parseCommentData(data) {
-  var parsedData = [];
-  var line = data.split('\n');
-  var pcount = 0;
-  var scount = 0;
-  
-  for (var i = 1; i < line.length; i++) {
-    var splitline = line[i].split('\t');
-      
-    if (splitline[0] !== '') {
-      pcount++;
-      scount = 0;
-      parsedData.push([splitline[0]]);
-  
-    } else if (splitline[1] !== '') {
-      scount++;
-      parsedData[pcount-1].push([splitline[1], []]);
-          
-    } else if (splitline[2] !== '') {
-      var p = parsedData[pcount-1];
-      var s = p[scount];
-      var text = splitline[2];
-      s[1].push(text);
-    }
-  }
-  
-  return parsedData;
-}
 
-function getNumPrimaryCategories(data) {
-  return data.length;
-}
+var internalCommentData = {
+ "data": [
+		{
+			"keyword": ["general", "common"],
+			"comment": "Open the quiz if you'd like to read more feedback"
+		},
+		{
+			"keyword": ["general", "common"],
+			"comment": "Don't forget to come back and respond to your classmates"
+		},
+		{
+			"keyword": ["general", "common"],
+			"comment": "Unfortunately I can't open files of that type in BlackBoard.  Please resubmit using either Microsoft format (.docx, .pptx, etc.) or PDF format (.pdf)"
+		},
+		{
+			"keyword": ["general", "common"],
+			"comment": "Let me know if you'd like to try this assignment again."
+		},
+		
+		{
+			"keyword": ["cp", "career planning", "assignments"],
+			"comment": "Be careful of spelling, punctuation and grammar.  You might consider installing a tool like Grammarly"
+		},
+		{
+			"keyword": ["cp", "career planning", "assignments"],
+			"comment": "Please resubmit, making sure to include the worksheet from the assignment"
+		},
+		{
+			"keyword": ["cp", "career planning", "db", "discussion"],
+			"comment": "Please respond to at least two of your classmates."
+		},
+		{
+			"keyword": ["cp", "career planning", "db", "discussion"],
+			"comment": "After you add to your post I'll update your score"
+		},
 
-function getNumSecondaryCategories(data, primaryIndex) {
-  return commentData[primaryIndex].length - 1;
-}
+		{
+			"keyword": ["vb", "visual basic", "assignments"],
+			"comment": "Please submit the .vb file for this assignment."
+		},
+		{
+			"keyword": ["vb","visual basic", "assignments"],
+			"comment": "I was unable to run your program.  Please fix the syntax errors and resubmit."
+		},
+		{
+			"keyword": ["vb","visual basic", "assignments"],
+			"comment": "Let me know if I can answer any questions."
+		},
+		{
+			"keyword": ["vb", "visual basic", "db", "discussion"],
+			"comment": "Please respond to at least two of your classmates."
+		},
+		{
+			"keyword": ["vb", "visual basic", "db", "discussion"],
+			"comment": "After you add to your post I'll update your score"
+		},
+		
+		{
+			"keyword": ["markup"],
+			"comment": "multi-line comment|continued on this line"
+		},
+		{
+			"keyword": ["markup"],
+			"comment": "A **bold** comment"
+		},
+		{
+			"keyword": ["markup"],
+			"comment": "An *italic* comment"
+		},
+		{
+			"keyword": ["markup"],
+			"comment": "A ***bold italic*** comment"
+		},
+		{
+			"keyword": ["markup"],
+			"comment": "This should be a `bcode block`e and this shouldnt. "
+		}
+	],
+	"keywords": []};
 
-function getNumComments(data, primaryIndex, secondaryIndex) { 
-  var primary = data[primaryIndex];
-  var secondary = primary[secondaryIndex + 1];
-  return secondary[1].length;
-}
+function parseCommentData()
+{
+	var keyset = new Set();
+	
+	for (var i = 0; i < internalCommentData.data.length; i++) {
+		var klist = internalCommentData.data[i].keyword;
+		for (var j = 0; j < klist.length; j++) {
+			keyset.add(klist[j]);
+		}
+	}
 
-function getPrimaryText(data, primaryIndex) {
-  return commentData[primaryIndex][0];
-}
+	var uniqueKeywords = [];
+	var  iterator1 = keyset.values();
+	for (let entry of iterator1) {
+		uniqueKeywords.push(entry);
+	}
 
-function getSecondaryText(data, primaryIndex, secondaryIndex) {
-  var primary = data[primaryIndex];
-  return primary[secondaryIndex+1][0];
-}
-
-function getCommentText(data, primaryIndex, secondaryIndex, commentIndex) {
-  var primary = data[primaryIndex];
-  var secondary = primary[secondaryIndex + 1];
-  return secondary[1][commentIndex];
+	console.log(uniqueKeywords.toString());
+	uniqueKeywords.sort();
+	internalCommentData.keywords = uniqueKeywords;
+	
+	return internalCommentData;
 }
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,208 +1,20 @@
 "use strict";
 
 var commentData;
-var selectIndex = {
-  "primary": 0,
-  "secondary": 0,
-  "comment": 0
-};
 
 $(document).ready(function() {
 
- // loadDataFromLocal($("#filename").html().trim());
-  loadDataFromServer();
-    
-  $("#selPrimary").change(function() {
-     handlePrimaryChange();
-  });
-
-  $("#selSecondary").change(function() {
-     handleSecondaryChange();
-  });
-
-  $("#selComment").change(function() {
-     handleCommentChange();
-  });
+  loadDataFromInternal();
   
   $("#copyTarget").hide();
+
   new Clipboard('.btn');
 });
 
-function loadDataFromLocal(filename) {  // from local file
-  console.log('loadData...');
-  var commentURL = chrome.runtime.getURL(filename);
-  console.log('made URL...');
-  $.ajax({
-    url: commentURL,
-    dataType: "text",
-    success: function(data) {
-      console.log('read data...');
-      commentData = parseCommentData(data);
-      initializeSelectIndices();
-    },
-    error: function() {
-      showError('unable to load from comment data file "' + filename + '"');
-    }
-  });
-}
-
-function loadDataFromServer() {
-	console.log('loadDataFromServer...');
-	var username = '';
-	chrome.storage.sync.get({"username":'unknown'}, function(items) {
-		console.log(JSON.stringify(items));
-		username = items.username;
-		console.log("user name = " + username);
-		if (username == 'unknown') {
-			console.log('no username specified');
-		} else {
-			var url = 'https://yheti.duckdns.org/commentbuddy/public/' + username;
-			console.log('url=' + url);
-			$.ajax({
-				url: url,
-				dataType: 'text',
-				success: function(data) {
-					console.log('success in loading comment data');
-					commentData = parseCommentData(data);
-					initializeSelectIndices();
-				},
-				error: function (error) {
-					console.log('failed to download comment data');
-				}
-			});
-		}	
-	});
-}
-
-function initializeSelectIndices() {
-  chrome.storage.local.get(['primaryIndex', 'secondaryIndex', 'commentIndex'], function(items) {
-    selectIndex.primary = 0;
-    selectIndex.secondary = 0;
-    selectIndex.comment = 0;
-
-    if (typeof items.primaryIndex != 'undefined') {
-      selectIndex.primary = items.primaryIndex;
-      selectIndex.secondary = items.secondaryIndex;
-      selectIndex.comment = items.commentIndex;
-    }
-    if (selectIndex.primary >= getNumPrimaryCategories(commentData)) {
-        selectIndex.primary = 0;
-        selectIndex.secondary = 0;
-        selectIndex.comment = 0
-    }
-    if (selectIndex.secondary >= getNumSecondaryCategories(commentData, selectIndex.primary)) {
-      selectIndex.secondary = 0;
-      selectIndex.comment = 0
-    }
-    if (selectIndex.comment >= getNumComments(commentData, selectIndex.primary, selectIndex.secondary)) {
-      selectIndex.comment = 0
-    }
-        
-    saveSelectIndices();
-
-    loadPrimary();
-    loadSecondary();
-    loadComment();
-    handleCommentChange();
-  });
-}
-
-function saveSelectIndices() {
-  chrome.storage.local.set(
-    {
-      "primaryIndex": selectIndex.primary, 
-      "secondaryIndex": selectIndex.secondary,
-      "commentIndex": selectIndex.comment
-    }, 
-    function() {}
-  );
-}
-
-function loadPrimary() {
-  var s = '';
-  var n = getNumPrimaryCategories(commentData);
-
-  for (var i = 0; i < n; i++) {
-    var text = getPrimaryText(commentData, i);
-    var fmt = "00" + i;
-    var num = fmt.substr(fmt.length-3);
-    var value = 'value="' + num + '" ';
-    var id = 'id="' + 'optPrimary' + num + '" ';
-    var selected = ' ';
-    if (i == selectIndex.primary)
-      selected = ' selected ';
-
-    s += '<option ' + value + id + selected + '>' + text + '</option>';
-  }
-
-  $("#selPrimary").html(s);
-}
-
-function loadSecondary() {
-  var s = '';
-  var n = getNumSecondaryCategories(commentData, selectIndex.primary);
-
-  for (var i = 0; i < n; i++) {
-    var text = getSecondaryText(commentData, selectIndex.primary, i);
-    var fmt = "00" + i;
-    var num = fmt.substr(fmt.length-3);
-    var value = 'value="' + num + '" ';
-    var id = 'id="' + 'optSecondary' + num + '" ';
-    var selected = ' ';
-    if (i == selectIndex.secondary)
-      selected = ' selected ';
-
-    s += '<option ' + value + id + selected + '>' + text + '</option>';
-  }
-
-  $("#selSecondary").html(s);
-}
-
-function loadComment() {
-  var s = '';
-  var n = getNumComments(commentData, selectIndex.primary, selectIndex.secondary);
-
-  for (var i = 0; i < n; i++) {
-    var text = getCommentText(commentData, selectIndex.primary, selectIndex.secondary, i);
-
-    var fmt = "00" + i;
-    var num = fmt.substr(fmt.length-3);
-    var value = 'value="' + num + '" ';
-    var selected = ' ';
-    if (i == selectIndex.comment)
-      selected = ' selected ';    
-    var id = 'id="' + 'optComment' + num + '" ';
-
-    s += '<option ' + value + id + selected + '>' + text + '</option>';
-  }
-
-  $("#selComment").attr("size", 20);
-  $("#selComment").html(s);
-}
-
-function handlePrimaryChange() {
-  selectIndex.primary = parseInt(currentOption('selPrimary'));
-  selectIndex.secondary = 0;
-  selectIndex.comment = 0;
-  saveSelectIndices();
-  loadSecondary();
-  loadComment();
-}
-
-function handleSecondaryChange() {
-  var primaryIndex = parseInt(currentOption('selPrimary'));
-  selectIndex.secondary = parseInt(currentOption('selSecondary'));
-  selectIndex.comment = 0;
-  saveSelectIndices();
-  loadComment();
-}
-
-function handleCommentChange() {
-  var option = currentOption('selComment');
-  selectIndex.comment = parseInt(option);
-  saveSelectIndices();
-
-  copyTextToClipboard($("#optComment" + option).html());
+function loadDataFromInternal() {
+  commentData = parseCommentData();
+	console.log(commentData.keywords.toString());
+  console.log("loadDataFromInternal complete");
 }
 
 function currentOption(id) {
@@ -216,26 +28,16 @@ function copyTextToClipboard(text) {
 	$("#copyTarget").html(formattedText);
 	$("#btnCopy").click();
 	$("#copyTarget").hide();
-	/*
-	var copyFrom = document.createElement("textarea");
-	copyFrom.textContent = formattedText;
-	var body = document.getElementsByTagName('body')[0];
-	body.appendChild(copyFrom);
-	copyFrom.select();
-	document.execCommand('copy');
-	body.removeChild(copyFrom);
-	*/
 }
 
 function formatTextFromMarkup(text) {
 	var codeblockspan = "<span style=\"font-family: 'courier new', courier;\">";
 	var codeblockendspan = '</span>';
-	console.log('text before=' + text);
+
 	var formattedText = text.replace(/\|/gi, ' \r\n');
 	var formattedText = formattedText.replace(/`b/gi, codeblockspan);
 	var formattedText = formattedText.replace(/`e/gi, codeblockendspan);
 	var formattedText = marked( formattedText );
-	console.log('text after=' + formattedText);  
 
 	return formattedText;
 }
@@ -243,4 +45,3 @@ function formatTextFromMarkup(text) {
 function showError(strError) {
   $("#spanError").html('<b> Error - ' + strError + '</b>');
 }
-
